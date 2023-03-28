bbfbe9f4448e381efc8ce93a45e6736e9b3d7b70
xDruid <savelyev.eg@gmail.com>
2023-03-28 16:12:53 +0700

    goto systemctl

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+apt update
+apt install python3 python3-pip -y
+apt install python3-opencv -y
+
+pip3 install -r requirements.txt
+
+mkdir /usr/bin/cctmk
+cp config.py /usr/bin/cctmk/
+cp cctmk.py /usr/bin/cctmk/
+cp app.py /usr/bin/cctmk/
+
+cp cctmk.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable cctmk.service
+systemctl start cctmk.service
